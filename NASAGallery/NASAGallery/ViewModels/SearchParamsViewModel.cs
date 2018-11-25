6fb41ae844c36642ce6a1f1df047b9079322c635
Oleksii Herasymov <krum78@gmail.com>
2018-11-25 17:27:38 +0200

    Code cleanup

--- NASAGallery/NASAGallery/ViewModels/SearchParamsViewModel.cs
+++ NASAGallery/NASAGallery/ViewModels/SearchParamsViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using NASAGallery.Common;
 using NASAGallery.Repository;
 using Xamarin.Forms;
 
--- NASAGallery/NASAGallery/ViewModels/SearchResultItemViewModel.cs
+++ NASAGallery/NASAGallery/ViewModels/SearchResultItemViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using NASAGallery.Repository;
 
 namespace NASAGallery.ViewModels
